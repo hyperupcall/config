605e03a89542f5498a784f2c5d1d1e53c4839218
Edwin Kofler <edwin@kofler.dev>
2022-08-11 19:01:21 -0700

    misc: Styling and fixes

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import { path, fs, yaml, toml } from "../mod.ts";
  * user options
  */
 interface ILoadOptions {
-  readonly searchDir?: string;
-  readonly file: string;
+	readonly searchDir?: string;
+	readonly file: string;
 }
 
 /**
@@ -13,60 +13,64 @@ interface ILoadOptions {
  */
 export type configFileFormat = "module" | "toml" | "json" | "yaml";
 interface IConfigFile {
-  readonly type: configFileFormat;
-  readonly fileName: string;
+	readonly type: configFileFormat;
+	readonly fileName: string;
 }
 
 export class Config {
-  private static async actuallyLoad(
-    { searchDir = Deno.cwd() }: ILoadOptions,
-    configFile: IConfigFile,
-  ): Promise<object | undefined> {
-    let configFilePath = path.join(searchDir, configFile.fileName);
-    if (Deno.build.os === "windows") configFilePath.slice(1);
+	private static async actuallyLoad(
+		{ searchDir = Deno.cwd() }: ILoadOptions,
+		configFile: IConfigFile
+	): Promise<Record<string, unknown> | undefined> {
+		let configFilePath = path.join(searchDir, configFile.fileName);
+		if (Deno.build.os === "windows") configFilePath.slice(1);
 
-    if (configFile.type === "module") {
-      if (Deno.build.os === "windows") {
-        configFilePath = ("\\" + configFilePath).replace("\\", "/");
-      }
-      const config = await import(configFilePath);
-      return config.default;
-    } else if (configFile.type === "toml") {
-      const content = await Deno.readTextFile(configFilePath);
-      return toml.parse(content);
-    } else if (configFile.type === "yaml") {
-      const content = await Deno.readTextFile(configFilePath);
-      return yaml.parse(content) as object;
-    } else if (configFile.type === "json") {
-      return (JSON.parse(await Deno.readTextFile(configFilePath))) as object;
-    }
-  }
+		if (configFile.type === "module") {
+			if (Deno.build.os === "windows") {
+				configFilePath = ("\\" + configFilePath).replace("\\", "/");
+			}
+			const config = await import(configFilePath);
+			return config.default;
+		} else if (configFile.type === "toml") {
+			const content = await Deno.readTextFile(configFilePath);
+			return toml.parse(content);
+		} else if (configFile.type === "yaml") {
+			const content = await Deno.readTextFile(configFilePath);
+			return yaml.parse(content) as Record<string, unknown>;
+		} else if (configFile.type === "json") {
+			return JSON.parse(await Deno.readTextFile(configFilePath)) as Record<
+				string,
+				unknown
+			>;
+		}
+	}
 
-  static async load(opts: ILoadOptions): Promise<object | undefined> {
-    const configFiles: ReadonlyArray<IConfigFile> = [
-      // first check config directory (which never prepends files with a dot)
-      { type: "module", fileName: `.config/${opts.file}.config.ts` },
-      { type: "module", fileName: `.config/${opts.file}.config.js` },
-      { type: "toml", fileName: `.config/${opts.file}.toml` },
-      { type: "json", fileName: `.config/${opts.file}.json` },
-      { type: "yaml", fileName: `.config/${opts.file}.yaml` },
-      { type: "yaml", fileName: `.config/${opts.file}.yml` },
-      // then check root directory
-      { type: "module", fileName: `${opts.file}.config.ts` },
-      { type: "module", fileName: `${opts.file}.config.js` },
-      { type: "toml", fileName: `.${opts.file}.toml` },
-      { type: "json", fileName: `.${opts.file}.json` },
-      { type: "yaml", fileName: `.${opts.file}.yaml` },
-      { type: "yaml", fileName: `.${opts.file}.yml` },
-    ];
+	static async load(
+		opts: ILoadOptions
+	): Promise<Record<string, unknown> | undefined> {
+		const configFiles: ReadonlyArray<IConfigFile> = [
+			// first check config directory (which never prepends files with a dot)
+			{ type: "module", fileName: `.config/${opts.file}.config.ts` },
+			{ type: "module", fileName: `.config/${opts.file}.config.js` },
+			{ type: "toml", fileName: `.config/${opts.file}.toml` },
+			{ type: "json", fileName: `.config/${opts.file}.json` },
+			{ type: "yaml", fileName: `.config/${opts.file}.yaml` },
+			{ type: "yaml", fileName: `.config/${opts.file}.yml` },
+			// then check root directory
+			{ type: "module", fileName: `${opts.file}.config.ts` },
+			{ type: "module", fileName: `${opts.file}.config.js` },
+			{ type: "toml", fileName: `.${opts.file}.toml` },
+			{ type: "json", fileName: `.${opts.file}.json` },
+			{ type: "yaml", fileName: `.${opts.file}.yaml` },
+			{ type: "yaml", fileName: `.${opts.file}.yml` },
+		];
 
-    searchForConfig:
-    for (const configFile of configFiles) {
-      try {
-        return await this.actuallyLoad(opts, configFile);
-      } catch {}
-    }
-    // no configuration files found
-    return void 0;
-  }
+		searchForConfig: for (const configFile of configFiles) {
+			try {
+				return await this.actuallyLoad(opts, configFile);
+			} catch {}
+		}
+		// no configuration files found
+		return void 0;
+	}
 }
--- src/config_test.ts
+++ src/config_test.ts
@@ -7,47 +7,47 @@ const { test } = Deno;
 let _dirname = path.dirname(new URL(import.meta.url).pathname);
 if (Deno.build.os == "windows") _dirname = _dirname.slice(1);
 const testDir = (fixturesDir: string): string =>
-  path.join(_dirname, "fixtures", fixturesDir);
+	path.join(_dirname, "fixtures", fixturesDir);
 
 const moduleFileEndings = ["js", "ts", "js-config", "ts-config"];
 for (const moduleFileEnding of moduleFileEndings) {
-  test({
-    name: `module find ${moduleFileEnding} config`,
-    async fn(): Promise<void> {
-      const config = await Config.load({
-        searchDir: testDir(moduleFileEnding),
-        file: "file",
-      });
+	test({
+		name: `module find ${moduleFileEnding} config`,
+		async fn(): Promise<void> {
+			const config = await Config.load({
+				searchDir: testDir(moduleFileEnding),
+				file: "file",
+			});
 
-      assertEquals(config, {
-        name: moduleFileEnding,
-      });
-    },
-  });
+			assertEquals(config, {
+				name: moduleFileEnding,
+			});
+		},
+	});
 }
 
 const dotFiles: string[] = [
-  "toml",
-  "yaml",
-  "yml",
-  "json",
-  "toml-config",
-  "yaml-config",
-  "yml-config",
-  "json-config",
+	"toml",
+	"yaml",
+	"yml",
+	"json",
+	"toml-config",
+	"yaml-config",
+	"yml-config",
+	"json-config",
 ];
 for (const dotFile of dotFiles) {
-  test({
-    name: `file find ${dotFile} config`,
-    async fn(): Promise<void> {
-      const config = await Config.load({
-        searchDir: testDir(dotFile),
-        file: "file",
-      });
+	test({
+		name: `file find ${dotFile} config`,
+		async fn(): Promise<void> {
+			const config = await Config.load({
+				searchDir: testDir(dotFile),
+				file: "file",
+			});
 
-      assertEquals(config, {
-        name: dotFile,
-      });
-    },
-  });
+			assertEquals(config, {
+				name: dotFile,
+			});
+		},
+	});
 }

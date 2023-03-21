7a2839b38e8254763b98046eb42c2e995c8bd305
Edwin Kofler <edwin@kofler.dev>
2023-03-21 02:31:13 -0700

    chore: Update std to `v0.178.0`

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as path from "https://deno.land/std@0.136.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.136.0/fs/mod.ts";
-export * as yaml from "https://deno.land/std@0.136.0/encoding/yaml.ts";
-export * as toml from "https://deno.land/std@0.136.0/encoding/toml.ts";
-export { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.178.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.178.0/fs/mod.ts";
+export * as yaml from "https://deno.land/std@0.178.0/encoding/yaml.ts";
+export * as toml from "https://deno.land/std@0.178.0/encoding/toml.ts";
+export { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
